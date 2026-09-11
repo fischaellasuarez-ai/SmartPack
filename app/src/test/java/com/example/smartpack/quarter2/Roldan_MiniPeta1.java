51101dd26277aeb91f024a01bf78147b1e77081b
Suarez_MiniPeta1 <fischaellasuarez@gmail.com>
2026-09-11 22:03:31 +0800

    Roldan_MiniPeta1

---
+++ app/src/test/java/com/example/smartpack/quarter2/Roldan_MiniPeta1.java
@@ -0,0 +1,18 @@
+package com.example.smartpack.quarter2;
+
+import org.junit.Test;
+
+public class Roldan_MiniPeta1 {
+    @Test
+    public void printMyProfile()  {
+        String myName = "Lore";
+        String petName = "Milo";
+        String favFood = "Noodles";
+        int myAge = 16;
+
+        System.out.println("<My DIGITAL Profile>");
+        System.out.println("Big greetings! You may call me " + myName + " and currently, I am " + myAge + " years old.");
+        System.out.println("I have a pet named " + petName + ".");
+        System.out.println("My comfort meal I eat often is " + favFood + ". ");
+    }
+}
