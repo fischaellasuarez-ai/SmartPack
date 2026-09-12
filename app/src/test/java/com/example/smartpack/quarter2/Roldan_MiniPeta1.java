120aadd96b8e10aa9d83710d4517698a5e9c40b0
lorewritten <laerrrrol@gmail.com>
2026-09-12 20:23:10 +0900

    Roldan_MiniPeta1

--- app/src/test/java/com/example/smartpack/quarter2/Roldan_MiniPeta1.java
+++ app/src/test/java/com/example/smartpack/quarter2/Roldan_MiniPeta1.java
@@ -13,6 +13,6 @@ public class Roldan_MiniPeta1 {
         System.out.println("<My DIGITAL Profile>");
         System.out.println("Big greetings! You may call me " + myName + " and currently, I am " + myAge + " years old.");
         System.out.println("I have a pet named " + petName + ".");
-        System.out.println("My comfort meal I eat often is " + favFood + ". ");
+        System.out.println("My comfort meal I eat often is " + favFood + ".");
     }
 }
