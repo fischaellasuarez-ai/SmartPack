e10ab62946594118fb4b01c65da8138e21743e69
Rufino <26-000125@mcstrayuman.edu.ph.>
2026-09-14 13:52:19 +0800

    Rufino_Minipeta2

---
+++ app/src/test/java/com/example/smartpack/quarter2/Rufino_MiniPeta2.java
@@ -0,0 +1,18 @@
+package com.example.smartpack.quarter2;
+
+import org.junit.Test;
+
+public class Rufino_MiniPeta1 {
+    @Test
+    public void printMyProfile() {
+        String myName = "Rufino";
+        String petName = "Yuri and Pogi";
+        String favFood = "FriedChicken";
+        int myAge = 15;
+
+        System.out.println("<My DIGITAL Profile>");
+        System.out.println("Hello! You may call me " + myName + " and currently, " + myAge + " years old.");
+        System.out.println("I have a 2 adorable cats named " + petName + ".");
+        System.out.println("the food i craved the most is " + favFood + ".");
+    }
+}
