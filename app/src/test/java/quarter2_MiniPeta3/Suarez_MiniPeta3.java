2a0231a44917decab05e8862fd803a08e7b30144
SHS PC <fischaellasuarez@gmail.com>
2026-09-14 14:52:55 +0800

    MiniPeta3

---
+++ app/src/test/java/quarter2_MiniPeta3/Suarez_MiniPeta3.java
@@ -0,0 +1,35 @@
+
+package quarter2_MiniPeta3;
+
+public class Suarez_MiniPeta3 {
+    public static void main(String[] args) {
+
+    class DeliverNotification {
+
+        //Stores the name of the teacher
+        String teacherName;
+
+        //Stores the subject name
+        String subjectName;
+
+        //Stores the notification name
+        String notifName;
+
+        //Stores the notification detail
+        String notifDetail;
+
+        //Stores the requirements
+        String notifRequirment;
+
+        //Stores the notification date
+        int notifDate;
+
+        //Stores the application name
+        String appName;
+
+        // Stores whether the Status of the  student is READY or NOT READY
+        boolean notifStatus = true; // true = READY, false = not READY
+
+ System.out.println
+    }
+}
