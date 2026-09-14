b84aa7ed6ddc6ed81ac2b2484be98e4777d48746
lore <laerrrrol@gmail.com>
2026-09-14 14:15:27 +0800

    Roldan_MiniPeta2

--- app/src/test/java/com/example/smartpack/quarter2/Roldan_MiniPeta2.java
+++ app/src/test/java/com/example/smartpack/quarter2/Roldan_MiniPeta2.java
@@ -2,7 +2,7 @@ package com.example.smartpack.quarter2;
 
 import org.junit.Test;
 
-public class Roldan_MiniPeta1 {
+public class Roldan_MiniPeta2 {
     @Test
     public void printMyProfile()  {
         String myName = "Lore";
@@ -13,6 +13,6 @@ public class Roldan_MiniPeta1 {
         System.out.println("<My DIGITAL Profile>");
         System.out.println("Big greetings! You may call me " + myName + " and currently, I am " + myAge + " years old.");
         System.out.println("I have a pet named " + petName + ".");
-        System.out.println("My comfort meal I eat often is " + favFood + ".");
+        System.out.println("My comfort meal I eat often is " + favFood + ". ");
     }
 }
