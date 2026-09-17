6e18bc8f31b05f9c8100e052d5f85be643bacbaa
Suarez_MiniPeta1 <fischaellasuarez@gmail.com>
2026-09-17 10:23:47 +0800

    Pecore_MiniPeta3

---
+++ app/src/test/java/com/example/smartpack/quarter2_MiniPeta3/Pecore_MiniPeta3.java
@@ -0,0 +1,33 @@
+package com.example.smartpack.quarter2_MiniPeta3;
+
+import java.util.Scanner;
+
+public class Pecore_MiniPeta3
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+
+        String subject;
+        String requirement;
+
+        System.out.print("Enter Subject: ");
+        subject = input.nextLine();
+
+        System.out.print("Enter Requirement: ");
+        requirement = input.nextLine();
+
+        if(subject.isEmpty() || requirement.isEmpty())
+        {
+            System.out.println("Upload Failed!");
+            System.out.println("Subject and Requirement must not be empty.");
+        }
+        else
+        {
+            System.out.println("Requirement Uploaded Successfully");
+            System.out.println("------------------------------");
+            System.out.println("Subject: " + subject);
+            System.out.println("Requirement: " + requirement);
+        }
+    }
+}
